dffbfd5f453185a938947352912808fe1af7be08
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-15 07:42:00 +0600

    dpne cpp operator

--- operator.cpp
+++ operator.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //https://www.w3schools.com/cpp/cpp_operators.asp
     /*
-      C++ Operators,
+      C++ Operators,Assignment Operators,Comparison Operators
     */
-
     cout<<"hello\n";
+
+    int a = 88;
+    int b = a+66;
+    int c = a + b;
+
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c<<"\n";
+
+    // cout<< a<b <<"\n"; bracket na dewai err diche
+    cout<< (a<b) <<"\n";
+    cout<< (b>c) <<"\n";
 
     return 0;
 }
